ea5009560038fc8a70d806e892216fb4411feded
cmpolik <grigorian.sa@phystech.edu>
2023-10-24 02:25:17 +0300

    1st HW AI RASU

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This is a short example for showing how Cookiecutter works',
+    author='Samvel Grigoryan',
+    license='MIT',
+)
